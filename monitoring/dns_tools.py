bbd453d6d34625815165edbd7e5e0bf6d89668ca
SeraphimUA <mikultvua@gmail.com>
2021-04-25 23:06:37 +0300

    Added check_dns

--- monitoring/dns_tools.py
+++ monitoring/dns_tools.py
@@ -1,22 +1,59 @@
 import dns.resolver
 from .config_loader import write_log
+from .check_utils import check_ip
+from .timestamp import timestamp
 
-def get_record(domain, rtype):
+# set resolver from config
+def get_resolver(config):
+    dns_timeout = config['timeouts']['dns']
+    my_resolver = dns.resolver.Resolver()
+    my_resolver.nameservers = config['dns']['resolvers']
+    return my_resolver
+
+# get dns records info
+def get_record(domain, rtype, my_resolver = None):
     a = []
     try:
-        answer = dns.resolver.resolve(domain, rtype)
+        # if resolver not defined, use system resolver
+        if my_resolver:
+            answer = my_resolver.resolve(domain, rtype)
+        else:
+            answer = dns.resolver.resolve(domain, rtype)
+
         for i in answer.response.answer:
             for j in i.items:
                 a.append(j.to_text())
 #    except dns.resolver.NoAnswer:
     except Exception as err_msg:
-        print(domain + " " + rtype + " not resolved. ")
-        print(err_msg)
+        err_msg = str(err_msg)
+        print(f"{domain} {rtype} not resolved. {err_msg}")
+        return None
     return a
 
-def get_ip_addresses(domain, check_ipv6=True):
-    a = get_record(domain, 'A')
+# get all IP addresses for domain (IPv4 & IPv6 (if set check_ipv6))
+def get_ip_addresses(domain, my_resolver = None, check_ipv6=True):
+    a = []
+    i = get_record(domain, 'A', my_resolver)
+    print(i)
+    if i:
+         for j in i:
+             if check_ip(j):
+                 a.append(j)    
     if check_ipv6:
-        for i in get_record(domain, 'AAAA'):
-            a.append(i)
+        i = get_record(domain, 'AAAA', my_resolver)
+        if i:
+            for j in i:
+                if check_ip(j):
+                    a.append(j)
     return a
+
+def check_dns(domain, my_resolver = None):
+    a = get_record(domain, 'A', my_resolver)
+    aaaa = get_record(domain, 'AAAA', my_resolver)
+    ns = get_record(domain, 'NS', my_resolver)
+    mx = get_record(domain, 'MX', my_resolver)
+    if a:
+        result = {'status': 'ok', 'A': a, 'AAAA': aaaa, 'NS': ns, 'MX': mx}
+    else:
+        result = {'status': 'fail', 'time_error': timestamp()}
+    return result
